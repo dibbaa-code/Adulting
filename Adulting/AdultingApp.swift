139ef0e1ae39fba5bc19b3eec85b2a69887f73a4
Divi157 <divyasaini6798@gmail.com>
2025-05-22 11:22:14 -0700

    Initial Commit

---
+++ Adulting/AdultingApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdultingApp.swift
+//  Adulting
+//
+//  Created by Divya Saini on 5/22/25.
+//
+
+import SwiftUI
+
+@main
+struct AdultingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AdultingTests/AdultingTests.swift
@@ -0,0 +1,17 @@
+//
+//  AdultingTests.swift
+//  AdultingTests
+//
+//  Created by Divya Saini on 5/22/25.
+//
+
+import Testing
+@testable import Adulting
+
+struct AdultingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
